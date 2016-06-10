13a2e8abdb8a6d785f860c67dc9b9161604bade9
Josh Starcher <josh.starcher@gmail.com>
2016-06-10 12:54:14 -0400

    Add syslog logging.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.logger = ActiveSupport::TaggedLogging.new(Logger::Syslog.new("canary-#{ENV['ENVIRONMENT']}", Syslog::LOG_LOCAL7))
+  config.log_tags = [->(request) { "ReqID:#{request.uuid}" }]
 end
