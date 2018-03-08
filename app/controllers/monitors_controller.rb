90295a8621b237a54cfa7ab5ed1f3cb2d0100303
Matt Drees <matt.drees@cru.org>
2018-03-08 14:17:30 -0700

    add monitors/lb route for health checks
    This path is required by our ALB tooling.

---
+++ app/controllers/monitors_controller.rb
@@ -0,0 +1,8 @@
+class MonitorsController < ApplicationController
+
+  layout nil
+
+  def lb
+    render plain: File.read(Rails.public_path.join('lb.txt'))
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   get 'main/hello'
 
+  get 'monitors/lb'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
