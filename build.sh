b95d0534815d1ab25f7980e8b2e55a1877d943c9
Brian Zoetewey <brian.zoetewey@cru.org>
2022-02-17 12:11:11 -0500

    Use docker buildx to build container.

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-docker build --build-arg DD_API_KEY=$DD_API_KEY \
-             --build-arg SIDEKIQ_CREDS=$SIDEKIQ_CREDS \
-             -t 056154071827.dkr.ecr.us-east-1.amazonaws.com/$PROJECT_NAME:$ENVIRONMENT-$BUILD_NUMBER .
+docker buildx build $DOCKER_ARGS \
+  --build-arg DD_API_KEY=$DD_API_KEY \
+  --build-arg SIDEKIQ_CREDS=$SIDEKIQ_CREDS \
+  .
