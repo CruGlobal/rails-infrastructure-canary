cb0ee2c2bdbe6a835cbb331b02d591cba6e2f3aa
Justin Sabelko <justin.sabelko@cru.org>
2025-09-25 14:16:48 -0500

    Upgrade to Sidekiq 7

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,8 +3,7 @@ require "datadog/statsd"
 
 redis_conf = YAML.safe_load(ERB.new(File.read(Rails.root.join("config", "redis.yml"))).result, permitted_classes: [Symbol], aliases: true)["sidekiq"]
 
-redis_settings = {url: Redis.new(redis_conf).id,
-                  namespace: redis_conf[:namespace]}
+redis_settings = {url: Redis.new(redis_conf).id}
 
 Sidekiq.configure_client do |config|
   config.redis = redis_settings
